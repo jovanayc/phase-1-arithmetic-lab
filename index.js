c20b2d75401cb8ad1a7fce9b325b26b4c203a755
Jovanay <jovanay.d.carter@gmail.com>
2021-12-24 17:44:37 -0500

    commit tree 38196e3f2d5a2702afe3e421155eebc55e1ca303

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+//Create multiply variable
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
+
+//Create random variable
+let random = Math.floor(Math.random() * 1000) + 1;
+console.log(random);
+
+//Create mod variable
+let num3 = 39
+let num4 = 7
+let mod = num3 % num4;
+console.log(mod);
+
+//Create max variable
+let max = Math.max(5,10,15,20);
+console.log(max);
+
